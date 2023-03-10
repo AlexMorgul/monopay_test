b54a60a1a201b698366a99ac3bf6d8a2f26b6e2a
AlexMorgul <alexander.morgul.work@gmail.com>
2023-03-10 20:57:29 +0200

    initial

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,20 @@
 let tg = window.Telegram.WebApp;
 
+tg.expand();
+
+tg.MainButton.show();
+tg.MainButton.setText("Сформувати посилання");
+
+Telegram.WebApp.onEvent("mainButtonClicked", function() {
+	let test = document.getElementById("test");
+	let p = document.createElement("p");
+
+	p.innerText("Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, rerum.");
+	test.appendChild(p);
+})
+
+// tg.MainButton.textColor = ;
+// tg.MainButton.color = ;
 
 
 function isPaymentExtended() {
