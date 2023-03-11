388e4af7b6326932dcdc6af5622c97830f84e0b9
AlexMorgul <alexander.morgul.work@gmail.com>
2023-03-12 00:44:18 +0200

    initial

--- scripts/app.js
+++ scripts/app.js
@@ -1,57 +1,37 @@
 
-// window.addEventListener( "load", function () {
-	let tg = window.Telegram.WebApp;
+let tg = window.Telegram.WebApp;
 
-	// tg.MainButton.show();
-	tg.MainButton.setText("СФОРМУВАТИ ПОСИЛАННЯ");
+tg.MainButton.setText("СФОРМУВАТИ ПОСИЛАННЯ");
 
-	const formElement = document.getElementById('myForm');
-	const chbox = document.getElementById('highload1');
-	const amount = document.getElementById('amount');
+const formElement = document.getElementById('myForm');
+const chbox = document.getElementById('highload1');
+const amount = document.getElementById('amount');
 
-	setColorScheme();
+setColorScheme();
 
-	formElement.addEventListener("submit", function (event) {
-		event.preventDefault();
-
-		const formData = new FormData(formElement);
-		
-		let data = {
-			"amount": Number(formData.get('amount'))
-		};
-
-		if (chbox.checked) {
-			data.basketOrder = [];
-
-			data.basketOrder[0] = {
-				"name": formData.get('itemName'),
-				"qty": Number(formData.get('quantity')),
-				"sum": Number(formData.get('amountPerItem'))
-			};
-		}
-
-		data = JSON.stringify(data);
-		sendData(data);
-	});
-
-	amount.addEventListener('input', function() {
-		this.value != '' ? tg.MainButton.show() : tg.MainButton.hide();
-	});
-
-	Telegram.WebApp.onEvent('themeChanged', function() {
-		setColorScheme();
-	});
+formElement.addEventListener("submit", function (event) {
+	event.preventDefault();
 	
-	Telegram.WebApp.onEvent('mainButtonClicked', function() {
-		const response = fetch("https://www.corezoid.com/api/2/json/public/1183175/3427feaf42c671b9b22a0d8a0c31169cb3e049df", {
-		method: 'POST',
-		headers: {},
-		body: data
-	});
-	});
+	let data = generateBodyRequest();
+	data = JSON.stringify(data);
+	sendData(data);
+});
 
-	tg.ready();
-// });
+amount.addEventListener('input', function() {
+	this.value != '' ? tg.MainButton.show() : tg.MainButton.hide();
+});
+
+Telegram.WebApp.onEvent('themeChanged', function() {
+	setColorScheme();
+});
+
+Telegram.WebApp.onEvent('mainButtonClicked', function() {
+	let data = generateBodyRequest();
+	data = JSON.stringify(data);
+	sendData(data);
+});
+
+tg.ready();
 
 function isPaymentExtended() {
 	let chbox = document.getElementById('highload1');
@@ -74,6 +54,26 @@ function setColorScheme() {
 	} else {
 		logo.src = "img/monopay_light_bg.svg";
 	}
+}
+
+function generateBodyRequest() {
+	const formData = new FormData(formElement);
+		
+	let data = {
+		"amount": Number(formData.get('amount'))
+	};
+
+	if (chbox.checked) {
+		data.basketOrder = [];
+
+		data.basketOrder[0] = {
+			"name": formData.get('itemName'),
+			"qty": Number(formData.get('quantity')),
+			"sum": Number(formData.get('amountPerItem'))
+		};
+	}
+
+	return data;
 }
 
 function sendData(data)  {
