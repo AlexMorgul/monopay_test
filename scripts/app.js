981ff091c083013d899d2c3996ae8394e3a8cbdd
AlexMorgul <alexander.morgul.work@gmail.com>
2023-03-16 10:05:43 +0200

    initial

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,7 @@ var nPositions = 0;
 
 const chbox = document.getElementById('highload1');
 const extented = document.getElementById('extented');
-const productFields = document.getElementById('productFields');
+const goods = document.getElementById('goods');
 var isExtend = false;
 
 const amount = document.getElementById('amount');
@@ -37,7 +37,6 @@ amount.addEventListener('input', function() {
 
 formElement.addEventListener("submit", function (event) {
 	event.preventDefault();
-	let data = generateBodyRequest();
 });
 
 Telegram.WebApp.onEvent('themeChanged', function() {
@@ -69,7 +68,7 @@ function validateValue() {
 			errors.innerHTML = '';
 			isDataValid = true;
 		} else {
-			errors.innerHTML = '<p style="color: red; margin-left: 11.25px;">- Перевірте вказані дані! (формат для вартості: 123.45)</p>';
+			errors.innerHTML = '<p style="color: red; margin-left: 11.25px; font-size: 13px;">- Перевірте вказані дані! (формат для вартості: 123.45)</p>';
 			isDataValid = false;
 		}
 	} else {
@@ -89,33 +88,105 @@ function isPaymentExtended() {
 	if (chbox.checked) {
 		extented.style.display = "";
 		isExtend = true;
-		nPositions = 1;
 
-		productFields.innerHTML = getFieldsTemplate();
+		if (nPositions == 0) {
+			nPositions = 1;
+			item = createItem();
+			goods.appendChild(item);
+		}
 	}
 	else {
 		extented.style.display = "none";
 		isExtend = false;
-		nPositions = 0;
-		productFields.innerHTML = '';
 	}
 }
 
 function addItem() {
 	nPositions++;
-	productFields.innerHTML += getFieldsTemplate();
+	item = createItem();
+	goods.appendChild(item);
 }
 
-function getFieldsTemplate() {
+function createItem() {
 	let indexName = 'itemName_' + nPositions;
 	let indexQuantity = 'quantity_' + nPositions;
 	let indexAmountPerItem = 'amountPerItem_' + nPositions;
 
-	return '<hr><p class="item_text">Товар №' + nPositions + '</p><label for="' + indexName + 
-		'">Назва товару:</label><input type="text" id="' + indexName + '" name="' + indexName + '"><br><br><label for="' 
-		+ indexQuantity + '">Кількість:</label><input type="text" id="'	+ indexQuantity + '" name="' + indexQuantity +
-		'"><br><br><label for="' + indexAmountPerItem + '">Сума за один товар:</label><input type="text" id="' + 
-		indexAmountPerItem + '" name="' + indexAmountPerItem + '"><br><br>';
+	const hr = document.createElement('hr');
+	const br = document.createElement('br');
+
+	// div 'item'
+	let div = document.createElement('div');
+	div.id = 'item_' + nPositions;
+	
+	// p 'item'
+	let p = document.createElement('p');
+	p.append('Товар №' + nPositions);
+	p.className = 'item_text';
+
+	// label & input 'name'
+	const labelItemName = document.createElement('label');
+	labelItemName.htmlFor = indexName;
+	labelItemName.append('Назва товару:');
+
+	const inputItemName = document.createElement('input');
+	Object.assign(inputItemName, {
+		type: 'text',
+		id: indexName,
+		name: indexName
+	})
+
+	// label & input 'quantity'
+	const labelQuantity = document.createElement('label');
+	labelQuantity.htmlFor = indexQuantity;
+	labelQuantity.append('Кількість:');
+
+	const inputQuantity = document.createElement('input');
+	Object.assign(inputQuantity, {
+		type: 'text',
+		id: indexQuantity,
+		name: indexQuantity
+	})
+
+	// label & input 'amountPerItem'
+	const labelAmountPerItem = document.createElement('label');
+	labelAmountPerItem.htmlFor = indexAmountPerItem;
+	labelAmountPerItem.append('Сума за один товар:');
+
+	const inputAmountPerItem = document.createElement('input');
+	Object.assign(inputAmountPerItem, {
+		type: 'text',
+		id: indexAmountPerItem,
+		name: indexAmountPerItem
+	})
+
+	// create & fill fragment
+	const item = document.createDocumentFragment();
+
+	item.appendChild(hr);
+	item.appendChild(p);
+
+	// item name
+	item.appendChild(labelItemName);
+	item.appendChild(inputItemName);
+	item.appendChild(br);
+	item.appendChild(br.cloneNode());
+
+	// item quantity
+	item.appendChild(labelQuantity);
+	item.appendChild(inputQuantity);
+	item.appendChild(br.cloneNode());
+	item.appendChild(br.cloneNode());
+
+	// amount per item
+	item.appendChild(labelAmountPerItem);
+	item.appendChild(inputAmountPerItem);
+	item.appendChild(br.cloneNode());
+	item.appendChild(br.cloneNode());
+
+	div.appendChild(item);
+
+	return div;
 }
 
 function setColorScheme() {
@@ -143,16 +214,20 @@ function generateBodyRequest() {
 
 		for (let i = 0; i < nPositions; i++) {
 			data.merchantPaymInfo.basketOrder[0] = {
-				"name": formData.get('itemName_' + i),
-				"qty": Number(formData.get('quantity_' + i)),
-				"sum": Number(formData.get('amountPerItem_' + i))
+				"name": formData.get('itemName_' + i+1),
+				"qty": Number(formData.get('quantity_' + i+1)),
+				"sum": Number(formData.get('amountPerItem_' + i+1))
 			};
 		}
-	}
-
-	console.log(data);
+	}	
 
 	data.queryId = tg.initDataUnsafe.query_id;
+
+	let obj = {};
+	formData.forEach((value, key) => obj[key] = value);
+	let json = JSON.stringify(obj);
+
+	console.log(json);
 
 	return data;
 }
